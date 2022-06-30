d6dcf035438edcdd79645475002c1d5d8be5fa5f
Jonathan Mani <jonathanmani@protonmail.com>
2022-06-30 11:25:09 -0400

    changed the server to change data to JSON and display to frontend

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const http = require('http')
 const app = http.createServer((req,res) =>{
-    res.writeHead(200,{ 'Content-Type': 'text/plain '})
-    res.end('Hello World')
+    res.writeHead(200,{ 'Content-Type': 'application/json'})
+    res.end(JSON.stringify(notes))
 })
 const PORT = 3000;
 app.listen(PORT)
 
 console.log(`Server running on Port ${PORT}`)
+
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2022-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2022-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
