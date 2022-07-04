6b9e07656555a59eb2c253364196ce4fe6e77855
Jonathan Mani <jonathanmani@protonmail.com>
2022-07-04 13:33:00 -0400

    created generateId() function and created note with specific properties

--- index.js
+++ index.js
@@ -51,12 +51,28 @@ app.delete('/api/notes/:id', (req,res)=>{
     res.status(204).end();
 })
 
-app.post('/api/notes', (req,res)=>{
+const generateId = () =>{
     const maxId = notes.length > 0 ? 
         Math.max(...notes.map(n => n.id)) : 0;
-    
-    const note = req.body;
-    note.id = maxId + 1;
+
+    return maxId + 1; 
+}
+
+app.post('/api/notes', (req,res)=>{
+    const body = req.body;
+
+    if(!body.content){
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
 
     notes = notes.concat(note);
 
